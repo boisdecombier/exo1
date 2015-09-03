95bb7db9ff67afeee3a0d90152ba5850c9967a64
Paul van Bommel <boisdecombier@gmail.com>
2015-09-03 14:42:06 +0200

    début de formulaire avec un post dans un fichier form

--- simple.rb
+++ simple.rb
@@ -25,3 +25,13 @@ end
 get '/contact' do	
 	erb :contact
 end 
+
+get '/form' do	
+	erb :form
+end 
+
+post '/form' do
+	@name = "#{params[:post][:first_name]} #{params[:post][:last_name]}"
+  @title = "Hello #{@name}"
+  erb :hello
+end
