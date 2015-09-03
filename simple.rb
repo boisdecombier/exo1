2186191cd9863f1f79d8e12797ff8b5da02d9b7b
Paul van Bommel <boisdecombier@gmail.com>
2015-09-03 12:12:35 +0200

    fichier simple.rb avec les routes

---
+++ simple.rb
@@ -0,0 +1,21 @@
+#!/home/paul/.rvm/gems/ruby-2.2.1/bin/shotgun
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+	erb :index	
+end
+
+get '/vente' do
+	erb :vente
+end
+
+get '/produits' do	
+	erb :produits
+end 
+get '/recettes' do	
+	erb :recettes
+end 
+get '/stage' do	
+	erb :stage
+end 
